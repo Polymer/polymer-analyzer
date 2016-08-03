d75d67231e33f323f02490901f209acf90ed3e57
Peter Burns <rictic@google.com>
2016-08-03 08:26:00 -0700

    Remove more custom typings

--- custom_typings/main.d.ts
+++ custom_typings/main.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="./doctrine.d.ts" />
 /// <reference path="./dom5.d.ts" />
 /// <reference path="./espree.d.ts" />
-/// <reference path="./estraverse.d.ts" />
 /// <reference path="./jsonschema.d.ts" />
 /// <reference path="./shady-css-parser.d.ts" />
