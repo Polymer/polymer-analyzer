ce60b6a6382b2ddcc4491d3b9a736e67bab67aef
Peter Burns <rictic@google.com>
2016-08-02 17:25:55 -0700

    Use estree from upstream

--- custom_typings/main.d.ts
+++ custom_typings/main.d.ts
@@ -3,6 +3,5 @@
 /// <reference path="./escodegen.d.ts" />
 /// <reference path="./espree.d.ts" />
 /// <reference path="./estraverse.d.ts" />
-/// <reference path="./estree.d.ts" />
 /// <reference path="./jsonschema.d.ts" />
 /// <reference path="./shady-css-parser.d.ts" />
