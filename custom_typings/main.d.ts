403d593edd947da208c6498cc6a233f6ed10829c
Peter Burns <rictic@google.com>
2016-08-03 08:27:34 -0700

    Update doctrine custom typings with the version that I'm upstreaming

--- custom_typings/main.d.ts
+++ custom_typings/main.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="./doctrine.d.ts" />
-/// <reference path="./dom5.d.ts" />
 /// <reference path="./espree.d.ts" />
 /// <reference path="./jsonschema.d.ts" />
 /// <reference path="./shady-css-parser.d.ts" />
