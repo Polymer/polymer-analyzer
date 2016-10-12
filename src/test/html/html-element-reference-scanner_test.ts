6a67bd3b401cb5a93cd16588cb0ce9cb382fe0eb
43081j <43081j@users.noreply.github.com>
2016-10-12 20:21:24 +0100

    add tests for SourceRanges of element refs

--- src/test/html/html-element-reference-scanner_test.ts
+++ src/test/html/html-element-reference-scanner_test.ts
@@ -3,18 +3,35 @@ import {HtmlVisitor} from '../../html/html-document';
 import {HtmlParser} from '../../html/html-parser';
 import {HtmlElementReferenceScanner} from '../../html/html-element-reference-scanner';
 import {ScannedElementReference} from '../../model/element-reference';
+import {WarningPrinter} from '../../warning/warning-printer';
+import {Analyzer} from '../../analyzer';
+import {SourceRange} from '../../model/model';
 
 suite('HtmlElementReferenceScanner', () => {
 
   suite('scan()', () => {
     let scanner: HtmlElementReferenceScanner;
+    let contents = '';
+    const loader = {
+      canLoad: () => true,
+      load: () => Promise.resolve(contents)
+    };
+    const warningPrinter = new WarningPrinter(
+      null as any, {analyzer: new Analyzer({urlLoader: loader})});
+
+    async function getUnderlinedText(sourceRange: SourceRange|undefined) {
+      if(!sourceRange) {
+        return 'No source range produced';
+      }
+      return '\n' + await warningPrinter.getUnderlinedText(sourceRange);
+    }
 
     setup(() => {
       scanner = new HtmlElementReferenceScanner();
     });
 
     test('finds custom element references', async() => {
-      let contents = `<html><body>
+      contents = `<html><body>
           <div>Foo</div>
           <x-foo a=5 b="test" c></x-foo>
           <div>
@@ -22,6 +39,7 @@ suite('HtmlElementReferenceScanner', () => {
           </div>
           <h1>Bar</h1>
         </body></html>`;
+
       const document = new HtmlParser().parse(contents, 'test-document.html');
       let visit = async(visitor: HtmlVisitor) => document.visit([visitor]);
 
@@ -40,43 +58,73 @@ suite('HtmlElementReferenceScanner', () => {
         ['c', '']
       ]);
 
-      assert.deepEqual(features[0].attributes[0], {
-        name: 'a',
-        nameSourceRange: {
-          end: {
-            column: 18,
-            line: 2
-          },
-          file: 'test-document.html',
-          start: {
-            column: 17,
-            line: 2
-          }
-        },
-        sourceRange: {
-          end: {
-            column: 20,
-            line: 2
-          },
-          file: 'test-document.html',
-          start: {
-            column: 17,
-            line: 2
-          }
-        },
-        value: '5',
-        valueSourceRange: {
-          end: {
-            column: 20,
-            line: 2
-          },
-          file: 'test-document.html',
-          start: {
-            column: 19,
-            line: 2
-          }
-        }
-      });
+      const sourceRanges = await Promise.all(features.map(async f => await getUnderlinedText(f.sourceRange)));
+
+      assert.deepEqual(sourceRanges, [
+        `
+          <x-foo a=5 b="test" c></x-foo>
+          ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`,
+        `
+            <x-bar></x-bar>
+            ~~~~~~~~~~~~~~~`
+      ]);
+
+      const attrRanges = await Promise.all(features.map(
+        async f => await Promise.all(f.attributes.map(
+          async a => await getUnderlinedText(a.sourceRange)))));
+
+      assert.deepEqual(attrRanges, [
+        [
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                 ~~~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                     ~~~~~~~~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                              ~`
+        ],
+        []
+      ]);
+
+      const attrNameRanges = await Promise.all(features.map(
+        async f => await Promise.all(f.attributes.map(
+          async a => await getUnderlinedText(a.nameSourceRange)))));
+
+      assert.deepEqual(attrNameRanges, [
+        [
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                 ~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                     ~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                              ~`
+        ],
+        []
+      ]);
+
+      const attrValueRanges = await Promise.all(features.map(
+        async f => await Promise.all(f.attributes.map(
+          async a => await getUnderlinedText(a.valueSourceRange)))));
+
+      assert.deepEqual(attrValueRanges, [
+        [
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                   ~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                       ~~~~~~`,
+          `
+          <x-foo a=5 b="test" c></x-foo>
+                              ~`
+        ],
+        []
+      ]);
     });
 
   });
