47a7fa637d6f9f0899d34c6046a4fc89ae61bc31
43081j <43081j@users.noreply.github.com>
2016-10-15 01:34:33 +0100

    remove redundant tests for polymer element

--- src/test/polymer/polymer-element-scanner_test.ts
+++ src/test/polymer/polymer-element-scanner_test.ts
@@ -1,5 +1,18 @@
-import {assert} from 'chai';
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
 
+import {assert} from 'chai';
 import {Visitor} from '../../javascript/estree-visitor';
 import {JavaScriptParser} from '../../javascript/javascript-parser';
 import {ScannedPolymerElement} from '../../polymer/polymer-element';
@@ -71,10 +84,8 @@ suite('PolymerElementScanner', () => {
 
       const features = await scanner.scan(document, visit);
 
-      assert.equal(features.length, 2);
-      assert.instanceOf(features[0], ScannedPolymerElement);
-      assert.instanceOf(features[1], ScannedPolymerElement);
       assert.deepEqual(features.map(f => f.tagName), ['x-foo', 'x-bar']);
+
       assert.deepEqual(
           features[0].observers.map(o => o.expression),
           ['_anObserver()', '_anotherObserver()']);
