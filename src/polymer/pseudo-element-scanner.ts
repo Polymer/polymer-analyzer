4b09fce02529b7149a8d01b80e005f41201dbf1e
Martin Kleinschrodt <martin@maklesoft.com>
2016-12-15 10:33:45 +0100

    Remove parsedPseudoElement function from docs module; add description to PseudoElementScanner

--- src/polymer/pseudo-element-scanner.ts
+++ src/polymer/pseudo-element-scanner.ts
@@ -22,6 +22,11 @@ import {HtmlVisitor, ParsedHtmlDocument} from '../html/html-document';
 import {HtmlScanner} from '../html/html-scanner';
 import {ScannedPolymerElement} from './polymer-element';
 
+/**
+ * A Polymer pseudo-element is an element that is declared in an unusual way, such
+ * that the analyzer couldn't normally analyze it, so instead it is declared in
+ * comments.
+ */
 export class PseudoElementScanner implements HtmlScanner {
   async scan(
       document: ParsedHtmlDocument,
